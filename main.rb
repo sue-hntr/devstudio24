6ffa2274ec4cd8d7429344ef5e4bd8781525c829
Suzanne Hunter <sue_huntr@yahoo.com>
2017-07-12 17:07:44 -0400

    return to HTML to save time

--- main.rb
+++ main.rb
@@ -1,8 +1,27 @@
 require 'sinatra'
 require 'bootstrap-sass'
 
-	get '/' do 
-		"worked on dreamhost"
+	get '/views/' do 
+		erb :index
 	end
 
+	get '/views/contact' do 
+		erb :contact
+	end
 	
+	get '/views/graphicdesign' do 
+		erb :graphicdesign
+	end
+
+	get '/views/responsive' do 
+		erb :responsive
+	end
+
+	get '/views/rubyrails' do 
+		erb :rubyrails
+	end
+
+	get '/views/samplewordpress' do 
+		erb :samplewordpress
+	end
+
